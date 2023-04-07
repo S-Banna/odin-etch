acdd23cf13448c72cb15ef61a089140a3cb71e0a
FlipFlop222 <bnsari789@gmail.com>
2023-04-07 18:45:45 +0300

    Good

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function generateGrid () {
     for (i = 0; i < jsrangeValue * jsrangeValue; i++) {
         divArr[i] = document.createElement("div");
         grid.appendChild(divArr[i]);
+        divArr[i].classList.add("click")
         if (jsrangeValue == 16) {
             divArr[i].classList.add("sixteen");
         }
@@ -23,4 +24,18 @@ function generateGrid () {
             console.log("error")
         }
     }
+    /*let clickEvent = document.querySelector(".click");
+    console.log(clickEvent);
+    for (j = 0; j <= clickEvent.length; j++) {
+        clickEvent[j].addEventListener("click", function() {
+            clickEvent[j].classList.add("black");
+    });
+    }*/
+    let eventClick = grid.childNodes;
+    console.log(eventClick);
+    var arr = Array.prototype.slice.call(eventClick);
+    console.log(arr);
+    arr.forEach(element => element.addEventListener("mousedown", function() {
+        element.classList.toggle("black");
+    }));
 }
